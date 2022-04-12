ad2b44faa38d0d8b2ccfe0e95775e8b79d737a0e
AdamRoomka <adam.romas12@gmail.com>
2022-04-12 17:34:00 +0300

    listCardMokinys

--- my-app/src/components/Tema3/Praktika1/ListCardMokinys.js
+++ my-app/src/components/Tema3/Praktika1/ListCardMokinys.js
@@ -1,18 +1,60 @@
 import React from "react";
 
+
 function ListCardMokinys({ id, vardas, pavarde, gm, miestas, programa, grupe, status}) {
 
   var gim = gm.toString().substr(0,10);
 
-  // var requestOptions = {
-  //   method: 'DELETE',
-  //   redirect: 'follow'
-  // };
-  
-  // fetch("http://localhost:3000/api/v1/students", requestOptions)
-  //   .then(response => response.text())
-  //   .then(result => console.log(result))
-  //   .catch(error => console.log('error', error));
+  const addStudent = (data) => {
+    if (editingId) {
+      // PUT METHOD fetch
+      fetch("http://localhost:4000/api/v1/students/" + editingId, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((res) => {
+          console.log(res);
+          getStudents();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      // POST METHOD
+      fetch("http://localhost:4000/api/v1/students", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((res) => {
+          console.log(res);
+          getStudents();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  //  PUT METHOD item
+  const editStudent = (data) => {
+    setEditingID(data._id);
+    setCurrentStudent(data);
+  };
+
+  // DELETE METHOD
+  const deleteStudent = (id) => {
+    fetch(`http://localhost:4000/api/v1/students/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log(res);
+      getStudents();
+    });
+  };
 
   return (
     <div className="col-12 d-flex my-3">
